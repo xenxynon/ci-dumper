cd86a41cf45e8c0e489bea4548a58c8942ca9f43
Mayur <flynryder427@gmail.com>
2022-05-22 10:37:40 +0530

    Update dump.sh

--- dump.sh
+++ dump.sh
@@ -1,3 +1,4 @@
 # example 
 # try again
-./p*/dumper.sh 'https://bigota.d.miui.com/V12.5.5.0.RJGINXM/surya_in_global_images_V12.5.5.0.RJGINXM_20220215.0000.00_11.0_in_ad869eee2b.tgz'
+apt install wget
+bash p*/dumper.sh 'https://bigota.d.miui.com/V12.5.5.0.RJGINXM/surya_in_global_images_V12.5.5.0.RJGINXM_20220215.0000.00_11.0_in_ad869eee2b.tgz'
