ca9eac1ab08f088f1ffde6a0b882a42450706369
Mayur <flynryder427@gmail.com>
2022-05-22 11:41:32 +0530

    Final

--- dump.sh
+++ dump.sh
@@ -2,7 +2,10 @@
 # try again
 apt install wget
 cd p*
-wget https://raw.githubusercontent.com/XenStuff/phoenix_firmware_dumper/main/dumper.sh 
+wget https://raw.githubusercontent.com/xenxynon/ci-dumper/main/.github_orgname
+wget https://raw.githubusercontent.com/xenxynon/ci-dumper/main/.github_token
+wget https://raw.githubusercontent.com/xenxynon/ci-dumper/main/.tg_chat
+wget https://raw.githubusercontent.com/xenxynon/ci-dumper/main/.tg_token
 chmod +x d*.sh
 bash du*.sh 'https://bigota.d.miui.com/V12.0.3.0.QEJMIXM/beryllium_global_images_V12.0.3.0.QEJMIXM_20201227.0000.00_10.0_global_f4a9dbd1d3.tgz'
 #bash p*/d*.sh 'https://bigota.d.miui.com/V12.5.5.0.RJGINXM/surya_in_global_images_V12.5.5.0.RJGINXM_20220215.0000.00_11.0_in_ad869eee2b.tgz'
