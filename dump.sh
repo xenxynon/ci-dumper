cb28fa8a5c695981f7c59fc02fb8c76f1830b172
Mayur <flynryder427@gmail.com>
2022-05-22 10:17:34 +0530

    okie let's try

--- dump.sh
+++ dump.sh
@@ -1,2 +1,2 @@
 # example
-# ./dumper.sh 'https://bigota.d.miui.com/V12.5.5.0.RJGINXM/surya_in_global_images_V12.5.5.0.RJGINXM_20220215.0000.00_11.0_in_ad869eee2b.tgz'
+./dumper.sh 'https://bigota.d.miui.com/V12.5.5.0.RJGINXM/surya_in_global_images_V12.5.5.0.RJGINXM_20220215.0000.00_11.0_in_ad869eee2b.tgz'
