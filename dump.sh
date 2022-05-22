aa41d3726571ab4c9f86cfbd92e218b60636086f
Mayur <flynryder427@gmail.com>
2022-05-22 11:47:05 +0530

    Update dump.sh

--- dump.sh
+++ dump.sh
@@ -7,5 +7,12 @@ wget https://raw.githubusercontent.com/xenxynon/ci-dumper/main/.github_token
 wget https://raw.githubusercontent.com/xenxynon/ci-dumper/main/.tg_chat
 wget https://raw.githubusercontent.com/xenxynon/ci-dumper/main/.tg_token
 chmod +x d*.sh
+
+# git config
+git config --global user.email "flynryder427@gmail.com"
+git config --global user.name "xenxynon"
+git config --global color.ui false
+
+# dump now
 bash du*.sh 'https://bigota.d.miui.com/V12.0.3.0.QEJMIXM/beryllium_global_images_V12.0.3.0.QEJMIXM_20201227.0000.00_10.0_global_f4a9dbd1d3.tgz'
 #bash p*/d*.sh 'https://bigota.d.miui.com/V12.5.5.0.RJGINXM/surya_in_global_images_V12.5.5.0.RJGINXM_20220215.0000.00_11.0_in_ad869eee2b.tgz'
