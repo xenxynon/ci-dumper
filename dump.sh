df354efff12e8de31eecf885040d97ae41207d68
Mayur <flynryder427@gmail.com>
2022-05-22 10:31:39 +0530

    Oke

--- dump.sh
+++ dump.sh
@@ -1,3 +1,3 @@
 # example 
 # try again
-./dumper.sh 'https://bigota.d.miui.com/V12.5.5.0.RJGINXM/surya_in_global_images_V12.5.5.0.RJGINXM_20220215.0000.00_11.0_in_ad869eee2b.tgz'
+./p*/dumper.sh 'https://bigota.d.miui.com/V12.5.5.0.RJGINXM/surya_in_global_images_V12.5.5.0.RJGINXM_20220215.0000.00_11.0_in_ad869eee2b.tgz'
