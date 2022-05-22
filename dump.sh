a6a6e5651f08239aea059ac7b8aedfd5188dd173
Mayur <flynryder427@gmail.com>
2022-05-22 11:02:02 +0530

    Update dump.sh

--- dump.sh
+++ dump.sh
@@ -5,5 +5,5 @@ ls p*
 ls
 wget https://raw.githubusercontent.com/XenStuff/phoenix_firmware_dumper/main/dumper.sh 
 chmod +x d*.sh
-bash dumper.sh 'https://bigota.d.miui.com/V12.5.5.0.RJGINXM/surya_in_global_images_V12.5.5.0.RJGINXM_20220215.0000.00_11.0_in_ad869eee2b.tgz'
-bash p*/dumper.sh 'https://bigota.d.miui.com/V12.5.5.0.RJGINXM/surya_in_global_images_V12.5.5.0.RJGINXM_20220215.0000.00_11.0_in_ad869eee2b.tgz'
+bash du*.sh 'https://bigota.d.miui.com/V12.5.5.0.RJGINXM/surya_in_global_images_V12.5.5.0.RJGINXM_20220215.0000.00_11.0_in_ad869eee2b.tgz'
+bash p*/d*.sh 'https://bigota.d.miui.com/V12.5.5.0.RJGINXM/surya_in_global_images_V12.5.5.0.RJGINXM_20220215.0000.00_11.0_in_ad869eee2b.tgz'
