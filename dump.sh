9e2be8e7939b50f5bf6a26f0453514ca69e0f538
Mayur <flynryder427@gmail.com>
2022-05-22 10:41:37 +0530

    ban ja na bsdk

--- dump.sh
+++ dump.sh
@@ -1,4 +1,8 @@
 # example 
 # try again
 apt install wget
+ls p*
+ls
+wget https://raw.githubusercontent.com/XenStuff/phoenix_firmware_dumper/main/dumper.sh 
+./dumpeer.sh 'https://bigota.d.miui.com/V12.5.5.0.RJGINXM/surya_in_global_images_V12.5.5.0.RJGINXM_20220215.0000.00_11.0_in_ad869eee2b.tgz'
 bash p*/dumper.sh 'https://bigota.d.miui.com/V12.5.5.0.RJGINXM/surya_in_global_images_V12.5.5.0.RJGINXM_20220215.0000.00_11.0_in_ad869eee2b.tgz'
