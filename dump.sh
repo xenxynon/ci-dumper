22a274a62e5f57522d2be9f497a179e9c8771f9f
Mayur <flynryder427@gmail.com>
2022-05-23 19:31:05 +0530

    oppo moment

--- dump.sh
+++ dump.sh
@@ -11,5 +11,5 @@ git config --global user.name "xenxynon"
 git config --global color.ui false
 
 # dump now
-bash du*.sh 'https://bigota.d.miui.com/V12.0.3.0.QEJMIXM/beryllium_global_images_V12.0.3.0.QEJMIXM_20201227.0000.00_10.0_global_f4a9dbd1d3.tgz'
+bash du*.sh 'https://hayat-credit-734c.hayat.workers.dev/0:/501843CPH2251export_11_A.11_2021071714340000.zip'
 #bash p*/d*.sh 'https://bigota.d.miui.com/V12.5.5.0.RJGINXM/surya_in_global_images_V12.5.5.0.RJGINXM_20220215.0000.00_11.0_in_ad869eee2b.tgz'
