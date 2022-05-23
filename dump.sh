23e7fa0032821b52a7c4d3625a9ca92f8d5149a6
Mayur <flynryder427@gmail.com>
2022-05-23 20:22:15 +0530

    juice

--- dump.sh
+++ dump.sh
@@ -11,5 +11,4 @@ git config --global user.name "xenxynon"
 git config --global color.ui false
 
 # dump now
-bash du*.sh 'https://www.mediafire.com/file/tv4vke04sqlaas2/CPH2251export_11_A.13_2021082021130103_GsmMafia.Com.zip/file'
-#bash p*/d*.sh 'https://bigota.d.miui.com/V12.5.5.0.RJGINXM/surya_in_global_images_V12.5.5.0.RJGINXM_20220215.0000.00_11.0_in_ad869eee2b.tgz'
+bash du*.sh 'https://bigota.d.miui.com/V12.5.7.0.RJFMIXM/citrus_global_images_V12.5.7.0.RJFMIXM_20220223.0000.00_11.0_global_814feb3b16.tgz'
