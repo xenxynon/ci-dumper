459447fe347b012c0a44515c3470146b29f863b4
Mayur <flynryder427@gmail.com>
2022-05-23 19:41:03 +0530

    Mido gaming

--- dump.sh
+++ dump.sh
@@ -1,7 +1,7 @@
 # example 
 # try again
 apt install wget
-cp .github_orgname .github_token .tg_chat .tg_token phoenix_firmware_dumper 
+cp .github_orgname .github_token .tg_chat .tg_token phoenix_firmware_dumper -v
 cd p*
 chmod +x d*.sh
 
@@ -11,5 +11,5 @@ git config --global user.name "xenxynon"
 git config --global color.ui false
 
 # dump now
-bash du*.sh 'https://hayat-credit-734c.hayat.workers.dev/0:/501843CPH2251export_11_A.11_2021071714340000.zip'
+bash du*.sh 'http://bigota.d.miui.com/V11.0.2.0.NCFMIXM/mido_global_images_V11.0.2.0.NCFMIXM_20191107.0000.00_7.0_global_91ed4d2a3f.tgz'
 #bash p*/d*.sh 'https://bigota.d.miui.com/V12.5.5.0.RJGINXM/surya_in_global_images_V12.5.5.0.RJGINXM_20220215.0000.00_11.0_in_ad869eee2b.tgz'
