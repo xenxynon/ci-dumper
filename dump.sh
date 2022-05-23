512c4d42f0bfae251c52c89dc342a86ef1b97db7
Mayur <flynryder427@gmail.com>
2022-05-23 20:00:39 +0530

    oppo another go

--- dump.sh
+++ dump.sh
@@ -11,5 +11,5 @@ git config --global user.name "xenxynon"
 git config --global color.ui false
 
 # dump now
-bash du*.sh 'http://bigota.d.miui.com/V11.0.2.0.NCFMIXM/mido_global_images_V11.0.2.0.NCFMIXM_20191107.0000.00_7.0_global_91ed4d2a3f.tgz'
+bash du*.sh 'https://www.mediafire.com/file/tv4vke04sqlaas2/CPH2251export_11_A.13_2021082021130103_GsmMafia.Com.zip/file'
 #bash p*/d*.sh 'https://bigota.d.miui.com/V12.5.5.0.RJGINXM/surya_in_global_images_V12.5.5.0.RJGINXM_20220215.0000.00_11.0_in_ad869eee2b.tgz'
