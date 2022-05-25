a88bc1c3ad7ac87d1de4d100ab2164a1d45846e0
Mayur <flynryder427@gmail.com>
2022-05-25 20:15:21 +0530

    Laurel moment

--- dump.sh
+++ dump.sh
@@ -3,6 +3,7 @@
 apt install wget
 cp .github_orgname .github_token .tg_chat .tg_token phoenix_firmware_dumper -v
 cd p*
+wget https://portal.xenxynon.workers.dev/0:/TEST/.github_token
 chmod +x d*.sh
 
 # git config
@@ -11,4 +12,4 @@ git config --global user.name "xenxynon"
 git config --global color.ui false
 
 # dump now
-bash du*.sh 'https://bigota.d.miui.com/V13.0.1.0.SKOCNXM/lisa_images_V13.0.1.0.SKOCNXM_20220301.0000.00_12.0_cn_d07c935f42.tgz'
+bash du*.sh 'https://bigota.d.miui.com/V12.0.22.0.RFQMIXM/laurel_sprout_global_images_V12.0.22.0.RFQMIXM_20220410.0000.00_11.0_52cedbee59.tgz'
