c8fa7907d58079e95946078011b3f456fd982acf
Mayur <flynryder427@gmail.com>
2022-06-25 13:59:03 +0530

    Blobs bhai

--- dump.sh
+++ dump.sh
@@ -12,4 +12,4 @@ git config --global user.name "xenxynon"
 git config --global color.ui false
 
 # dump now
-bash du*.sh 'https://bigota.d.miui.com/V12.5.7.0.QFGCNXM/lavender_images_V12.5.7.0.QFGCNXM_20211109.0000.00_10.0_cn_8b85782ef6.tgz'
+bash du*.sh 'https://cool-mouse-12c5.shubhamxd.workers.dev/0:/LA.UM.9.6.2.r1-03600-89xx.0.zip'
