17dcb0b48b92dada96c41f38787b314d33deadce
Mayur <flynryder427@gmail.com>
2022-07-04 12:46:59 +0530

    K

--- dump.sh
+++ dump.sh
@@ -12,4 +12,4 @@ git config --global user.name "xenxynon"
 git config --global color.ui false
 
 # dump now
-bash du*.sh 'https://polished-disk-a3d6.shubhamxd.workers.dev/0:/project-404-5.0-20220626-052357-SHINKA-GAPPS-lavender.zip'
+bash du*.sh 'https://sourceforge.net/projects/jabiyeff-build/files/BSP/LA.UM.9.6.3.r1-06200-89xx.0.zip/download'
