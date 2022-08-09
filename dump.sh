c8082d59ef8ead658d4baf2729c60577d38dfc1b
xenxynon <flynryder427@gmail.com>
2022-08-10 05:01:23 +0530

    init script
    Signed-off-by: xenxynon <flynryder427@gmail.com>

---
+++ dump.sh
@@ -0,0 +1,13 @@
+apt install wget
+cp .github_orgname .github_token .tg_chat .tg_token phoenix_firmware_dumper -v
+cd p*
+wget https://portal.xenxynon.workers.dev/0:/TEST/.github_token
+chmod +x d*.sh
+
+# git config
+git config --global user.email "flynryder427@gmail.com"
+git config --global user.name "xenxynon"
+git config --global color.ui false
+
+# change the link here and done
+bash du*.sh 'https://polished-disk-a3d6.shubhamxd.workers.dev/0:/AOSPA-sapphire-1-lavender-UNOFFICIAL-20220707.zip'
