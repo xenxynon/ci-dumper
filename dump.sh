9d9b386b38c96565bdcbe72144cb505d5eabc434
Mayur <flynryder427@gmail.com>
2022-08-20 09:06:21 +0530

    Gayrow

--- dump.sh
+++ dump.sh
@@ -12,4 +12,4 @@ git config --global user.name "xenxynon"
 git config --global color.ui false
 
 # dump now
-bash du*.sh 'https://polished-disk-a3d6.shubhamxd.workers.dev/0:/Pixel%20Hybrid%2013%20Beta%204%20July%20Lavender.zip'
+bash du*.sh 'https://polished-disk-a3d6.shubhamxd.workers.dev/0:/Arrow-v12.1-lavender-UNOFFICIAL-20220809-GAPPS.zip'
