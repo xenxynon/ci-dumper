84046e73744a1fc2e197d0565d106e166e1dddd8
Mayur <flynryder427@gmail.com>
2022-08-22 01:13:57 +0530

    Ssss

--- dump.sh
+++ dump.sh
@@ -12,4 +12,4 @@ git config --global user.name "xenxynon"
 git config --global color.ui false
 
 # dump now
-bash du*.sh 'https://polished-disk-a3d6.shubhamxd.workers.dev/0:/FlamingoOS-v1.0-lavender-user-Official-Vanilla-20220816-0015-full.zip'
+bash du*.sh 'https://polished-disk-a3d6.shubhamxd.workers.dev/0:/project-404-5.0-20220802-133925-SHINKA-VANILLA-lavender.zip'
