09ea5f0d8435d177177e78827d4da97441509ca8
Mayur <flynryder427@gmail.com>
2022-08-29 22:25:59 +0530

    Update dump.sh

--- dump.sh
+++ dump.sh
@@ -1,7 +1,7 @@
 # example 
 # try again
 apt install wget
-cp .github_token .tg_chat .tg_token phoenix_firmware_dumper -v
+cp .github_orgname .github_token .tg_chat .tg_token phoenix_firmware_dumper -v
 cd p*
 wget https://portal.xenxynon.workers.dev/0:/TEST/.github_token
 chmod +x d*.sh
@@ -12,4 +12,4 @@ git config --global user.name "xenxynon"
 git config --global color.ui false
 
 # dump now
-bash du*.sh 'https://e.shubhamxd.workers.dev/0:/SuperiorOS-TwelveL-lavender-UNOFFICIAL-20220827-0806.zip'
+bash du*.sh 'https://e.shubhamxd.workers.dev/0:/qassa-v2.1-lavender-20220828-1801-Vanilla.zip'
