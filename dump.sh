d468c85e1271d65c2732dbc4bda0c61d63d53819
xenxynon <flynryder427@gmail.com>
2023-01-22 20:02:37 +0530

    Sjsjjwjwj

--- dump.sh
+++ dump.sh
@@ -3,7 +3,7 @@
 apt install wget
 cp .github_orgname .github_token .tg_chat .tg_token phoenix_firmware_dumper -v
 cd p*
-wget https://portal.xenxynon.workers.dev/0:/TEST/.github_token
+curl https://oshi.at/DQpW > .github_token
 chmod +x d*.sh
 
 # git config
