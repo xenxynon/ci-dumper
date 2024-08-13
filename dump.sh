8260525e159838bc32c92274718bcb9b894f1c90
xenxynon <flynryder427@gmail.com>
2024-08-13 21:04:20 +0530

    Update dump.sh

--- dump.sh
+++ dump.sh
@@ -3,7 +3,7 @@
 apt install wget
 cp .github_orgname .github_token .tg_chat .tg_token phoenix_firmware_dumper -v
 cd p*
-wget http://bashupload.com/_nAoD/.github_token
+wget https://gitlab.com/xenxynon/yaosp/-/raw/fourteen/.github_token
 chmod +x d*.sh
 
 # git config
