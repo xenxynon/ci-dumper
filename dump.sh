24a1d02b0acbff6d90856232b6f5d827c1db9e8f
xenxynon <flynryder427@gmail.com>
2024-08-13 20:35:26 +0530

    Update dump.sh

--- dump.sh
+++ dump.sh
@@ -3,7 +3,7 @@
 apt install wget
 cp .github_orgname .github_token .tg_chat .tg_token phoenix_firmware_dumper -v
 cd p*
-curl http://oshi.at/NAwC > .github_token
+wget http://bashupload.com/_nAoD/.github_token
 chmod +x d*.sh
 
 # git config
@@ -13,5 +13,5 @@ git config --global color.ui false
 
 # dump now
 bash du*sh 'https://index.g4ruda.live/0:/PenguinOS-unity-phone1-20240805-0755-image.zip'
-curl -T "*/system_ext/priv-app/Settings/Settings.apk.aa" oshi.at
-curl -T "*/system_ext/priv-app/Settings/Settings.apk.ab" oshi.at
+curl -T "out/*/system_ext/priv-app/Settings/Settings.apk.aa" oshi.at
+curl -T "out/*/system_ext/priv-app/Settings/Settings.apk.ab" oshi.at
