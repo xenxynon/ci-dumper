5b09d5fc192ab9484c67a0f03da4eeaff99e236e
xenxynon <flynryder427@gmail.com>
2024-10-10 21:09:41 +0530

    Update dump.sh

--- dump.sh
+++ dump.sh
@@ -12,5 +12,13 @@ git config --global user.email "flynryder427@gmail.com"
 git config --global user.name "xenxynon"
 git config --global color.ui false
 
-# dump now
-bash dumper.sh 'https://index.g4ruda.live/0:/RisingOS-5.2.1-EOL-CORE-UNOFFICIAL-sweet.zip'
+#dumpyara
+git clone https://github.com/sebaubuntu-python/dumpyara yy
+cd yy && pip3 install dumpyara
+wget https://index.g4ruda.live/0:/RisingOS-5.2.1-EOL-CORE-UNOFFICIAL-sweet.zip
+python3 -m RisingOS-5.2.1-EOL-CORE-UNOFFICIAL-sweet.zip
+cd RisingOS-5.2.1-EOL-CORE-UNOFFICIAL-sweet
+curl -T system_ext/priv-app/*SystemUI*/*apk oshi.at > o.txt 
+cat o.txt
+curl -T system_ext/priv-app/*SystemUI*/*apk bashupload.com > b.txt
+cat b.txt
